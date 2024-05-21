270406afd006a6cf12068eaf4f3ab07e0ceb7060
_.tuanngp._ <nguyengiaphuongtuan@gmail.com>
2024-05-21 17:17:15 +0700

    init project

---
+++ Digital_Tome/src/main/java/com/fpt/swp391/group6/DigitalTome/entity/PaymentEntity.java
@@ -0,0 +1,49 @@
+package com.fpt.swp391.group6.DigitalTome.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.Date;
+
+@Entity
+@Table(name = "payment")
+public class PaymentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "price", nullable = false, precision = 10, scale = 2)
+    private BigDecimal decimal;
+
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {
+            CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name = "account_id")
+    private AccountEntity accountEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {
+            CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name = "book_id")
+    private BookEntity bookEntity;
+
+
+    @Column(name = "created_date", updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Column(name = "created_by", updatable = false)
+    private Long createdBy;
+
+    @Column(name = "modified_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date modifiedDate;
+
+    @Column(name = "modified_by")
+    private Long modifiedBy;
+
+    public PaymentEntity() {
+    }
+}
