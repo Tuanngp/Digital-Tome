b8e6612091a83bc0a3ed083d8ad08578bd461290
lekhanhduc <141480376+lekhanhduc@users.noreply.github.com>
2024-07-17 19:59:48 +0700

    config

--- src/main/java/com/fpt/swp391/group6/DigitalTome/config/SpringSecurity.java
+++ src/main/java/com/fpt/swp391/group6/DigitalTome/config/SpringSecurity.java
@@ -43,12 +43,12 @@ public class SpringSecurity {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/uploadbook/**").hasAnyRole("PUBLISHER")
                         .requestMatchers("/publisher/**").hasAnyRole("PUBLISHER", "ADMIN")
-<<<<<<< HEAD
+
                         .requestMatchers("/censor/**").hasAnyRole("CENSOR", "ADMIN")
                        /* .requestMatchers("/admin/").hasAnyRole("ADMIN")*/
-=======
+
                         .requestMatchers("/admin").hasAnyRole("ADMIN")
->>>>>>> e177050 (update premium)
+
                         .requestMatchers("/admin/**").permitAll()
                         .requestMatchers("/profile/**").authenticated()
                         .requestMatchers("/buypoint/**").authenticated()
